2babc5cebb126ecc8e089ae3d1739f74dcd1c355
boumster <95254000+boumster@users.noreply.github.com>
2024-01-26 22:38:13 +0000

    fixes

--- Lab1/ex2.6.py
+++ Lab1/ex2.6.py
@@ -13,7 +13,7 @@ def fact_list_comprehension(num):
     ans = [ans[-1] for i in range(1, num+1) if not ans.append(i*ans[-1] if ans else 1)]
     return ans[-1]
 
-elapsed_time = timeit.timeit(lambda: fact(5), number=1000)
+elapsed_time = timeit.timeit(lambda: fact(100), number=1000)
 print(f"Time taken for fact: {elapsed_time} seconds")
-elapsed_time = timeit.timeit(lambda: fact_list_comprehension(5), number=1000)
+elapsed_time = timeit.timeit(lambda: fact_list_comprehension(100), number=1000)
 print(f"Time taken for fact_list_comprehension: {elapsed_time} seconds")
